643178e4bc8246c0d0f1e44d9034b6f14673ce04
iobk <iobkyb@gmail.com>
2022-06-13 06:44:59 +0900

    add/mul package

---
+++ add.go
@@ -0,0 +1,12 @@
+package add
+
+import "fmt"
+
+func Add(a, b int) int {
+	fmt.Println("add package Add function -----")
+	return a + b
+}
+func Add1(a, b int) int {
+	fmt.Println("add package Add1 function -----")
+	return a + b
+}
